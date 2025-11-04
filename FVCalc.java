51ad3ae1034e2a5ea8f5bea223b2465783409439
roni.daniel01 <roni.daniel01@post.runi.ac.il>
2025-11-04 20:14:31 +0200

    current code of exercises

--- FVCalc.java
+++ FVCalc.java
@@ -1,15 +1,15 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
-	public static void main(String[] args){
-		// Replace this comment with your code
+    public static void main(String[] args){
+        // Replace this comment with your code
         int currentValue = Integer.parseInt(args[0]);
         double rate = Double.parseDouble(args[1]);
         int n = Integer.parseInt(args[2]);
 
 
 
-        double futureValue = currentValue* Math.pow(1+rate, (double)n );
-        System.out.println("After "+ n + " years, $" + currentValue + " saved at " + rate + "% will yield $" + (int)futureValue/100);
+        double futureValue = currentValue* Math.pow(1+rate/100.0, (double)n );
+        System.out.println("After "+ n + " years, $" + currentValue + " saved at " + rate + "% will yield $" + (int)futureValue);
 
 
         /*
@@ -17,5 +17,5 @@ public class FVCalc {
         After 25 years, $25000 saved at 6.0% will yield $107296
          */
 
-	}
+    }
 }
---
+++ PrintTo5.java
@@ -0,0 +1,20 @@
+// Prints the numbers 0 to 5.
+// (Same program as shown in lecture 1-1).
+public class PrintTo5 {
+
+    public static void MyFunc() {
+        System.out.println("Inside MyFunc!");
+    }
+
+	public static void main(String[] args) {
+		// Declares an int variable, and sets it to 1
+        int i = 0;
+        System.out.println("Hello I'm Roni! And i is " + i);
+        i = i + 1;
+        System.out.println("Hello I'm Roni! And i is " + i);
+        MyFunc();
+        System.out.println("After Call to MyFunc");
+        
+        // Complete the program's code here:
+    }
+ }

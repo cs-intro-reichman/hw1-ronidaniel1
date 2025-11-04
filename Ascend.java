5ec36be3dfa09fc7d4fca62edffa7c8029a8096d
roni.daniel01 <roni.daniel01@post.runi.ac.il>
2025-11-04 19:05:55 +0200

    current code of exercises

--- Ascend.java
+++ Ascend.java
@@ -1,7 +1,17 @@
 // Generates three integer random numbers in a given range,
 // and prints them in increasing order.
 public class Ascend {
-	public static void main(String[] args) {
-		// Replace this comment with your code
-	}
+    public static void main(String[] args) {
+        int lim = Integer.parseInt(args[0]);
+        int a = (int) (Math.random() * lim);
+        int b = (int) (Math.random() * lim);
+        int c = (int) (Math.random() * lim);
+        System.out.println(a + " " + b + " " + c);
+
+        int d = Math.min(a, b);
+        int e = Math.min(d, c);
+        int f = Math.max(a, b);
+        int g = Math.max(f, c);
+        int h = a + b + c - e - g;
+    }
 }
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,10 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int Num = Integer.parseInt(args[0]);
+        int hundreds = Num/100;
+        int tens = Num/10%10;
+        int ones = Num%10;
+        System.out.println(hundreds +" hundreds"+", "+ tens+ " tens" +", and " +ones+  " ones.");
 	}
 }
